5d98a2103b2fa63005bf584596f3fd9f9541a0e3
sergey <sergey@kinescope.io>
2025-11-21 10:13:47 +0400

    remove comments

--- cypress/e2e/cheerio-scraper-e2e.cy.js
+++ cypress/e2e/cheerio-scraper-e2e.cy.js
@@ -1,16 +1,6 @@
 import PublicWebsitePage from '../pages/publicWebsite'
 import ConsolePage from '../pages/consolePage'
 
-/**
- * End-to-End тест для Cheerio Scraper
- * 
- * Покрывает полный flow:
- * 1. Поиск актора на Public Website
- * 2. Открытие в Apify Console
- * 3. Обновление input
- * 4. Запуск актора
- * 5. Проверка результатов
- */
 describe('Cheerio Scraper E2E Flow', () => {
   beforeEach(() => {
     // Блокируем аналитику для ускорения тестов
@@ -20,14 +10,14 @@ describe('Cheerio Scraper E2E Flow', () => {
   it('should search, configure, run Cheerio Scraper and verify results', () => {
     const actorName = 'Cheerio Scraper Actor'
     
-    // Шаг 1: Поиск актора на Public Website
+    // Search for actor on Public Website
     PublicWebsitePage
       .visit()
       .searchForActor(actorName)
       .openActorCard('Cheerio Scraper')
       .openInConsole() // Явный переход на console.apify.com
     
-    // Шаг 2: Работа в Apify Console
+    // Apify Console 
     ConsolePage
       .waitForPageLoad()
       .verifyOnConsolePage()
