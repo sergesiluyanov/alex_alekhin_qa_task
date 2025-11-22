f027e6fcd8b4779f218c73951af0827df7ba2244
sergey <sergey@kinescope.io>
2025-11-22 19:56:13 +0400

    add resurrect run sequence

---
+++ cypress/e2e/cheerio-scraper-resurrect-run.cy.js
@@ -0,0 +1,30 @@
+import PublicWebsitePage from '../pages/publicWebsite'
+import ConsolePage from '../pages/consolePage'
+
+describe('Cheerio Scraper E2E Flow', () => {
+  beforeEach(() => {
+    // block analytics requests for faster tests
+    cy.blockAllRequests()
+    
+    // login to console (session is saved and reused)
+    cy.loginToConsole()
+  })
+
+  it('should search, configure, run and abort Cheerio Scraper', () => {
+    // Search for actor on Public Website
+    PublicWebsitePage
+      .visit()
+      .searchForActor()
+      .openActor()
+      .openInConsole()
+    
+    // Apify Console
+    ConsolePage
+      .waitForConfiguration()
+      .startActor()
+      .abortRun()
+      .verifyAbortConfirmation()
+      .verifyAbortMessage()
+  })
+})
+
